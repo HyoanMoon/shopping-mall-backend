2087d8918434f12b6453d338efab1ea71e4ec4a0
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-07 03:07:47 +0900

    implement pagenation function

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-
+const PAGE_SIZE = 5;
 const productController = {};
 
 productController.createProduct = async (req,res) => {
@@ -30,10 +30,24 @@ productController.getProducts = async (req,res) => {
     try{
         const {page,name} = req.query
         const cond = name?{name:{$regex: name, $options: 'i'}} : {}
-        let query = Product.find(cond)
+        let query = Product.find(cond);
+        let response = {status: "Success"};
+        if(page){
+            query.skip((page-1) * PAGE_SIZE ).limit(PAGE_SIZE) 
+            // limit() - 한페이지에 내가 몇개을 보여주고 싶은지
+            // skip() - 만약 3페이지를 보여주고 싶으면 (3-1) * 한 페이지 당 데이터 갯수를 곱해서 그만큼 데이터를 스킵하면 내가 원하는 페이지 데이터가 나옴. ex) 2*5 = 10개 건너뛰고 나머지 5개만 보여주면 3페이지. 
 
-        const productList = await query.exec()  // query를 실행시키고 싶을때 또는 실행을 따로 하고 싶을때 
-        res.status(200).json({ status: 'Success', data: productList });
+            // 데이터가 최종 몇개 있는지
+            const totalItemNum = await Product.find(cond).count()
+            // 데이터 총 개수 / PAGE_SIZE
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE); //전체 페이지 = 전체 데이터 수 / 내 페이지 사이즈
+            response.totalPageNum = totalPageNum
+
+        }
+
+        const productList = await query.exec()  // query를 실행시키고 싶을때 또는 실행을 따로 하고 싶을때
+        response.data= productList
+        res.status(200).json(response);
 
     }
     catch(error){
