94b7c606aef5750a8c3402dad06e66e78c9fdb14
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-10 05:41:55 +0900

    implement delete && modify item qty && total item qty function

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,3 +1,4 @@
+const { populate } = require("dotenv");
 const Cart = require("../models/Cart")
 const cartController = {};
 
@@ -22,7 +23,7 @@ cartController.addItemToCart = async (req, res) => {
             (item) => item.productId.equals(productId) && item.size === size);
         // productId type이  mongoose.ObjectId 이기 때문에 equals 함수로 비교해야 한다.
         if (existItem) {
-            throw new Error("Items alreay exist.")
+            throw new Error("Item alreay exist.")
         }
         // 2. 새로운 아이템을 카트에 추가
         cart.items = [...cart.items, { productId, size, qty }]
@@ -31,9 +32,95 @@ cartController.addItemToCart = async (req, res) => {
 
 
     } catch (error) {
-        res.status(400).json({ status: 'Fail..', message: error.message });
+        return res.status(400).json({ status: 'Fail..', message: error.message });
     }
 }
+cartController.getCartList = async (req, res) => {
+    try {
+        const { userId } = req;
+        const cartList = await Cart.findOne({ userId }).populate({ // items 안에 있는 productId를 가지고 Product를 가지고 온다.
+            path: 'items',
+            populate: {
+                path: 'productId',
+                model: 'Product'
+            }
+        });
+        res.status(200).json({ status: "Success", data: cartList.items });
+
+    } catch (error) {
+        return res.status(400).json({ status: 'Fail..', message: error.message });
+    }
+}
+
+// cartController.deleteCartItem = async (req, res) => {
+//     try {
+//         const cartId = req.params.cartId; // cart의 ID
+//         const itemIdToDelete = req.params.itemId; // 삭제할 item의 ID
+
+//         const updatedCart = await Cart.findByIdAndUpdate(
+//             cartId,
+//             { $pull: { items: { _id: itemIdToDelete } } },
+//             { new: true }
+//         );
+
+//         if (!updatedCart) throw new Error("Cart or item doesn't exist");
+
+//         res.status(200).json({ status: 'Success', data: updatedCart });
+//     } catch (error) {
+//         return res.status(400).json({ status: 'Fail..', message: error.message });
+//     }
+// }
+
+cartController.deleteCartItem = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId } = req;
+      const cart = await Cart.findOne({ userId });
+      cart.items = cart.items.filter((item) => !item._id.equals(id));
+  
+      await cart.save();
+      res.status(200).json({ status: "Success", cartItemQty: cart.items.length });
+    } catch (error) {
+      return res.status(400).json({ status: "fail", error: error.message });
+    }
+  };
+
+  cartController.editCartItem = async (req, res) => {
+    try {
+      const { userId } = req;
+      const { id } = req.params;
+  
+      const { qty } = req.body;
+      const cart = await Cart.findOne({ userId }).populate({
+        path: "items",
+        populate: {
+          path: "productId",
+          model: "Product",
+        },
+      });
+      if (!cart) throw new Error("There is no cart for this user");
+      const index = cart.items.findIndex((item) => item._id.equals(id));
+      if (index === -1) throw new Error("Can not find item");
+      cart.items[index].qty = qty;
+      await cart.save();
+      res.status(200).json({ status: 200, data: cart.items });
+    } catch (error) {
+      return res.status(400).json({ status: "fail", error: error.message });
+    }
+  };
+
+  cartController.getCartQty = async (req, res) => {
+    try {
+      const { userId } = req;
+      const cart = await Cart.findOne({ userId: userId });
+      if (!cart) throw new Error("There is no cart!");
+      res.status(200).json({ status: 200, qty: cart.items.length });
+    } catch (error) {
+      return res.status(400).json({ status: "fail", error: error.message });
+    }
+  };
+  
+
 
 
 
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -7,15 +7,18 @@ const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
 
+router.get("/", authController.authenticate, cartController.getCartList);
 
+// router.delete("/:cartId/item/:itemId", authController.authenticate, cartController.deleteCartItem)
 
+router.delete("/:id",
+    authController.authenticate,
+    cartController.deleteCartItem
+);
 
+router.put("/:id", authController.authenticate, cartController.editCartItem);
 
-
-
-
-
-
+router.get("/qty", authController.authenticate, cartController.getCartQty);
 
 
 
